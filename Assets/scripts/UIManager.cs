fc58074492af8f0864666fb760a2448ec46e6a9b
callistoAshley <callistoashley@proton.me>
2021-10-21 13:18:15 +1100

    ui stuff

---
+++ Assets/scripts/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace spacegame
+{
+    // REMEMBER THIS IS ATTACHED TO THE CANVAS PREFAB
+    public class UIManager : MonoBehaviour
+    {
+        public static UIManager instance;
+        // ui input queue
+
+        void Awake()
+        {
+            instance = this;
+        }
+    }
+}
