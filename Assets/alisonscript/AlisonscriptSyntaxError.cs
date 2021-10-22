003aac435e757e1480ebdc25aab0711c0d812e58
callistoAshley <callistoashley@proton.me>
2021-10-22 18:58:18 +1100

    bug fix

---
+++ Assets/alisonscript/AlisonscriptSyntaxError.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spacegame.alisonscript
+{
+    public class AlisonscriptSyntaxError : Exception
+    {
+        private static string ErrorText(int line) => $"alisonscript syntax error on line {line}: ";
+
+        public AlisonscriptSyntaxError() : base() { }
+        public AlisonscriptSyntaxError(int line, string message) : base (ErrorText(line) + message) { }
+        public AlisonscriptSyntaxError(int line, string message, Exception innerException) : base(ErrorText(line) + message, innerException) { }
+    }
+}
