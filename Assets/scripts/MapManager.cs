dba24e2dc7f97a7bbb2088652814ef000252bbac
callistoAshley <callistoashley@proton.me>
2021-11-06 12:00:22 +1100

    testing, bug fix

--- Assets/scripts/MapManager.cs
+++ Assets/scripts/MapManager.cs
@@ -16,8 +16,10 @@ namespace spacegame
         public static void ChangeMap(string name, int transferPoint = 0)
         {
             // load the scene
-            SceneManager.LoadScene(name);
-
+            AsyncOperation a = SceneManager.LoadSceneAsync(name);
+            a.completed += new Action<AsyncOperation>(
+                // call map data init as callback
+                x => MapData.map.Init(transferPoint));
             // do map init
             //MapData.map.Init(transferPoint);
         }
