03b80901bf68dcba69949b75e26b0a089a3fcc9d
callistoAshley <callistoashley@proton.me>
2022-01-18 20:34:31 +1100

    fix a bug where the player will ascend ladders slower if they interact with one while falling

--- Assets/scripts/Ladder.cs
+++ Assets/scripts/Ladder.cs
@@ -23,6 +23,10 @@ namespace spacegame
 
             // set the player's gravity to 0 so they can move up the ladder
             Player.instance.SetGravity(0);
+
+            // if the player interacts with the ladder while falling and their velocity isn't reset, their falling velocity will stay despite
+            // their gravity being 0, so they will ascend the ladder slower
+            Player.instance.rigidbody2d.velocity = Vector2.zero;
         }
 
         private void LeaveLadder(InputManager.KeyPressedEventArgs e)
